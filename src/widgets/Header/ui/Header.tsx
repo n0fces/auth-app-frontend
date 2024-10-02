4a4d7802cb08b345b5cf2a1b39647b79e3a79a3a
n0fces <ilyaabzalov@gmail.com>
2024-10-02 13:35:59 +0300

    fix not found page for spa application

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -13,20 +13,20 @@ interface HeaderProps {
 export const Header = ({ className }: HeaderProps) => {
 	const context = useSession();
 
-	return (
-		!context?.isLoading && (
-			<header className={className}>
-				<Card
-					direction="column"
-					className={styles.header}
-					maxWidth
-					alignItems="center">
-					<div className={styles.navigate}>
-						<NavigationItems />
-					</div>
-					<LogoutBtn className={styles.logout} />
-				</Card>
-			</header>
-		)
+	return context?.isLoading ? (
+		<Card className={styles.header} />
+	) : (
+		<header className={className}>
+			<Card
+				direction="column"
+				className={styles.header}
+				maxWidth
+				alignItems="center">
+				<div className={styles.navigate}>
+					<NavigationItems />
+				</div>
+				<LogoutBtn className={styles.logout} />
+			</Card>
+		</header>
 	);
 };
